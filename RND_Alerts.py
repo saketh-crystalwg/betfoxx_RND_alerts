52044200bf56c61bc92f21650cff85cd95ce9b94
saketh-crystalwg <saketh@crystalwg.com>
2024-11-13 16:34:14 +0000

    addition of camila21quiceno@gmail.com

--- RND_Alerts.py
+++ RND_Alerts.py
@@ -145,7 +145,7 @@ if customers is not None and customers.shape[0] > 0:
     body = f"Hi,\n\n Attached contains the during the hour of  {end_datetime} (UTC) for Betfoxx \n\nThanks,\nSaketh"
     sender = "sakethg250@gmail.com"
     recipients = ["saketh@crystalwg.com","ron@crystalwg.com","camila@crystalwg.com","celeste@crystalwg.com","cristina@crystalwg.com","lina@crystalwg.com","erika@crystalwg.com","isaac@crystalwg.com",
-    "sakethg250@gmail.com","alberto@crystalwg.com", "ximena@crystalwg.com","camila21quiceno@gmail.com"]
+    "sakethg250@gmail.com","alberto@crystalwg.com", "ximena@crystalwg.com","camila.betcoco@gmail.com"]
     password = "xjyb jsdl buri ylqr"
     send_mail(sender, recipients, subject, body, "smtp.gmail.com", 465, sender, password, valid_filename)
     
